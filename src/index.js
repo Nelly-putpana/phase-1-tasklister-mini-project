60b6d1ef0e8c7b7aa2c824f147f32d6104f8dc3a
Nelly Putpana <nelly.putpana@gmail.com>
2023-05-21 06:50:22 -0600

    displays task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners()
 });
+
+
+// 1. get the form element and attach an
+//  event listener to the form
+
+function addingEventListeners() {
+  document
+  .getElementById("create-task-form")
+  .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e) {
+e.preventDefault()
+// console.log(e)
+const task = e.target["new-task-description"].value
+
+displayTask(task)
+
+}
+
+function displayTask(task) {
+  const taskUl = document.getElementById("tasks")
+  
+  const taskLi = document.createElement("li")
+  
+  taskLi.textContent = task
+  taskUl.appendChild(taskLi)
+  
+ }
